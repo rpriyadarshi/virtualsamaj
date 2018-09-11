445da9a7f50de2c87b51fa0499916fd569cabc08
“Rohit <rohit@virtualsamaj.com>
2018-09-11 00:20:47 -0700

    Added animation example

---
+++ sensor/ADT7420/animation.py
@@ -0,0 +1,55 @@
+"""
+Emulate an oscilloscope.  Requires the animation API introduced in
+matplotlib 1.0 SVN.
+"""
+
+import numpy as np
+from matplotlib.lines import Line2D
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from ADT7420 import *
+
+
+class Scope:
+    def __init__(self, ax, maxt=2, dt=0.02):
+        self.ax = ax
+        self.dt = dt
+        self.maxt = maxt
+        self.tdata = [0]
+        self.ydata = [0]
+        self.line = Line2D(self.tdata, self.ydata)
+        self.ax.add_line(self.line)
+        self.ax.set_ylim(-.1, 150.1)
+        self.ax.set_xlim(0, self.maxt)
+
+    def update(self, y):
+        lastt = self.tdata[-1]
+        if lastt > self.tdata[0] + self.maxt: # reset the arrays
+            self.tdata = [self.tdata[-1]]
+            self.ydata = [self.ydata[-1]]
+            self.ax.set_xlim(self.tdata[0], self.tdata[0] + self.maxt)
+            self.ax.figure.canvas.draw()
+
+        t = self.tdata[-1] + self.dt
+        self.tdata.append(t)
+        self.ydata.append(y)
+        self.line.set_data(self.tdata, self.ydata)
+        return self.line,
+
+
+def emitter():
+    while True:
+        ts.read_once()
+        yield ts.temp
+
+
+ts = ADT7420(1, 0x49, .240, '/home/pi/temp_mon.csv', False)
+fig, ax = plt.subplots()
+scope = Scope(ax)
+
+# pass a generator in "emitter" to produce data for the update func
+ani = animation.FuncAnimation(fig, scope.update, emitter, interval=240,
+    blit=True)
+
+
+plt.show()
